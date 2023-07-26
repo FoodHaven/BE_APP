8a75931e7c5198fe08d192f00eae5082cbcdaf21
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-07-26 17:12:36 -0600

    build: working transit api call

--- app/services/transit_service.rb
+++ app/services/transit_service.rb
@@ -1,13 +1,14 @@
 class TransitService
   def self.conn
     Faraday.new(url: 'https://external.transitapp.com/v3') do |f|
-      f.headers['api_key'] = ENV['transit_api_key']
+      f.headers['apiKey'] = ENV['transit_api_key']
     end
   end
 
   def self.get_trips(origin_lat, origin_lon, destination_lat, destination_lon)
-    require 'pry'; binding.pry
-    JSON.parse(conn.get("otp/plan?fromPlace=#{origin_lat}, #{origin_lon}&toPlace=#{destination_lat}, #{destination_lon}"), symbolize_names: true)
+    response = conn.get("otp/plan?fromPlace=#{origin_lat}, #{origin_lon}&toPlace=#{destination_lat}, #{destination_lon}")
+    data = JSON.parse(response.body, symbolize_names: true)
+
   end
 end
 
