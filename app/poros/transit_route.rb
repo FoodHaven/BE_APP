c0e4e128f9537134db1bcce8254686a4b19bce6a
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-07-26 19:16:21 -0600

    build: transit poro

---
+++ app/poros/transit_route.rb
@@ -0,0 +1,13 @@
+class TransitRoute
+  attr_reader :agency_name, :route_short_name, :headsign, :start_time, :end_time
+
+  def initialize
+    def initialize(agency_name, route_short_name, headsign, start_time, end_time)
+      @agency_name = agency_name
+      @route_short_name = route_short_name
+      @headsign = headsign
+      @start_time = start_time
+      @end_time = end_time
+    end
+  end
+end
