9eec8ea3cdae007eaa1bf6f1d2cbc0ad507ecb73
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-07-26 19:34:15 -0600

    build: transit_routes_controller

--- app/poros/transit_route.rb
+++ app/poros/transit_route.rb
@@ -1,13 +1,11 @@
 class TransitRoute
   attr_reader :agency_name, :route_short_name, :headsign, :start_time, :end_time
 
-  def initialize
-    def initialize(agency_name, route_short_name, headsign, start_time, end_time)
-      @agency_name = agency_name
-      @route_short_name = route_short_name
-      @headsign = headsign
-      @start_time = start_time
-      @end_time = end_time
-    end
+  def initialize(agency_name, route_short_name, headsign, start_time, end_time)
+    @agency_name = agency_name
+    @route_short_name = route_short_name
+    @headsign = headsign
+    @start_time = start_time
+    @end_time = end_time
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :markets, only: [:index, :show] do
+        get 'routes', to: 'transit_routes#index'
+      end
+    end
+  end
   resources :users
 end
+
