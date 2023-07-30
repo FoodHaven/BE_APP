2d2aad445a903064231b21ea96769188026c7e5e
Jesse Sorman <jessesorman@gmail.com>
2023-07-30 17:01:46 -0400

    make small changes for FE

--- app/controllers/api/v1/transit_routes_controller.rb
+++ app/controllers/api/v1/transit_routes_controller.rb
@@ -1,9 +1,9 @@
-# module Api
-#   module V1
-#     class TransitRoutesController < ApplicationController
-#       def index
-#         routes = TransitFacade.get_trips(params[:original_lat], params[:original_lon], params[:destination_lat], params[:destination_lon])
-#       end
-#     end
-#   end
-# end
+module Api
+  module V1
+    class TransitRoutesController < ApplicationController
+      def index
+        routes = TransitFacade.get_trips(params[:original_lat], params[:original_lon], params[:destination_lat], params[:destination_lon])
+      end
+    end
+  end
+end
