555784af997820d9db0b664646e9d3dd52e70797
Jesse Sorman <jessesorman@gmail.com>
2023-08-27 13:00:24 -0400

    patch service path to heroku deployment

--- app/services/market_service.rb
+++ app/services/market_service.rb
@@ -1,6 +1,6 @@
 class MarketService
   def conn
-    Faraday.new(url: "https://foodhaven-farmers-markets-api.onrender.com")
+    Faraday.new(url: "https://foodhaven-df0d805938ff.herokuapp.com/")
   end
 
   def get_url(url)
@@ -9,16 +9,16 @@ class MarketService
   end
 
   def all_markets(latitude, longitude, radius)
-    get_url("/api/v1/markets/search?latitude=#{latitude}&longitude=#{longitude}&radius=#{radius}")
+    get_url("markets/search?latitude=#{latitude}&longitude=#{longitude}&radius=#{radius}")
   end
 
   def one_market(id)
-    get_url("/api/v1/markets/#{id}")
+    get_url("markets/#{id}")
   end
 
   def favorite_markets(ids)
     params = {market_ids: ids}
-    response = conn.get("/api/v1/markets/favorites", params)
+    response = conn.get("markets/favorites", params)
     JSON.parse(response.body, symbolize_names: true)
   end
 end
